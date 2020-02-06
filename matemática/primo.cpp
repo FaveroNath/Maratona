b75b1fd869791256eaddb41347b29ddef0430efd
FaveroNath <54823335+FaveroNath@users.noreply.github.com>
2020-02-06 15:52:52 -0300

    Update primo.cpp

--- matemática/primo.cpp
+++ matemática/primo.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool eh_primo(int n){
-  if(n == 1 return false;
+  if(n == 1) return false;
   for(int i = 2; i*i <= n; ++i){
     if(n%i == 0) return false;
   }
