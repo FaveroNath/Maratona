f18fe4a1955aaa5a4941d813013179f00a1655ec
FaveroNath <54823335+FaveroNath@users.noreply.github.com>
2020-02-06 15:51:48 -0300

    Create primo.cpp

---
+++ matemática/primo.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool eh_primo(int n){
+  if(n == 1 return false;
+  for(int i = 2; i*i <= n; ++i){
+    if(n%i == 0) return false;
+  }
+    return true;
+}
+int main(){
+  int n;
+  cin >> n;
+  if(eh_primo(n)) cout << "S";
+  else cout << "N";
+}
