fe17b02a662749c478fe9db5be1efc3489b8c0b3
FaveroNath <54823335+FaveroNath@users.noreply.github.com>
2020-02-21 11:08:35 -0300

    Update crivoDeErastotenes.cpp

--- matemática/crivoDeErastotenes.cpp
+++ matemática/crivoDeErastotenes.cpp
@@ -1,29 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int max = 100010;
-int eh_primo[10010000];
-vector<int> arr;
+const int N = 1e6;
+int eh_primo[N+5];
+typedef long long ll;
+
 void crivo(int n){
-	for (int i = 2; i <= n; i++){
-		eh_primo[i] = -1;
-	}
-	for(int i = 2; i <= n; i++){
-		if(eh_primo[i] == -1){
-			eh_primo[i] = i;
-			for (int j = i+i; j <= n; j += i){
-				eh_primo[j] = 0;
-			}
-		}
-	}
+    memset(eh_primo, -1, sizeof(eh_primo));
+    eh_primo[0] = eh_primo[1] = 0;
+    
+    for(int i = 2; i <= n; i++){
+        if(eh_primo[i] == -1){
+            eh_primo[i] = 1;
+            cout << i << " ";
+            for(int j = 2; i*j <= n; j++){
+                eh_primo[i*j] = 0;
+            }
+        }
+    }
 }
+
+
 int main(){
-	int prim;
-	cin >> prim;
-	crivo(prim);
-	for(int i = 1; i <= prim; i++){
-		if(eh_primo[i] != 0) arr.push_back(eh_primo[i]);
-	}
-	for(int i = 0; i < arr.size(); i++){
-	    cout << arr[i] << " ";
-	}
+	
+	int n; cin >> n;
+    	crivo(n);
+	return 0;
 }
